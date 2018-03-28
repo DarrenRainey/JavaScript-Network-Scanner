9533938c11c519d1dbe4e99fb2729b77a7d57e9b
Darren Rainey <darren@darrenraineys.co.uk>
2018-03-28 09:02:36 +0100

    Update data.php

--- data.php
+++ data.php
@@ -2,5 +2,5 @@
 <?php
 $data = $_GET["data"];
 $line = date('d-m-Y H:i:s') . "	-	Device: $data 		-	Origin: $_SERVER[REMOTE_ADDR]	-	User-Agent: $_SERVER[HTTP_USER_AGENT]";
-file_put_contents('nrc.log', $line . PHP_EOL, FILE_APPEND);
+file_put_contents('scan.log', $line . PHP_EOL, FILE_APPEND);
 ?>
